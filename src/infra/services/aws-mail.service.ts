82a09457a3980be09b9c289103f9f420bde83bb6
tcardosoc <tcardosoc.dev@gmail.com>
2023-11-11 10:55:36 -0300

    feat: add new service to handle with aws especific functions

---
+++ src/infra/services/aws-mail.service.ts
@@ -0,0 +1,49 @@
+import { mailTemplates } from "src/core/mail";
+import { aws } from "../../config/aws.config";
+import AWS from 'aws-sdk';
+import { ISendMailPayload } from "src/core/interfaces/mail.interfaces";
+
+class AmazonSimpleEmailService {
+    static async createOrUpdateTemplates() {
+        mailTemplates.map((template) => {
+            aws.updateTemplate(template)
+                .promise()
+                .then(
+                    () => console.log(`--- TEMPLATE DE EMAIL ${template.Template.TemplateName} ATUALIZADO ---`),
+                    async (err: AWS.AWSError) => {
+                        if (err.code = 'TemplateDoesNotExist') {
+                            await aws.createTemplate(template).promise().then(
+                                () => console.log(`--- TEMPLATE DE EMAIL ${template.Template.TemplateName} CRIADO ---`),
+                                (err: AWS.AWSError) => console.log(err, err.stack)
+
+                            )
+                        }
+                    }
+                )
+        })
+    }
+
+    static async sendEmail(payload: ISendMailPayload) {
+        const mailParams: AWS.SES.SendTemplatedEmailRequest = {
+            Destination: {
+                ToAddresses: [
+                    payload.to
+                ]
+            },
+            Source: '"tcardosoc" <tcardosoc.dev@gmail.com>',
+            Template: payload.templateName,
+            TemplateData: JSON.stringify(payload.templateData)
+        }
+
+        return await aws.sendTemplatedEmail(mailParams).promise()
+    }
+
+    static async getTemplates() {
+        const allTemplates = aws.listTemplates({ MaxItems: 99 })
+
+        return allTemplates;
+    }
+
+}
+
+export default AmazonSimpleEmailService
