57623d85f08e9453a8631f23e6c8e7530b6d50d7
Thiago Conte <tcr2@cin.ufpe.br>
2023-11-14 20:11:31 -0300

    refactor(TASK-01): userData type added

--- src/infra/services/auth.service.ts
+++ src/infra/services/auth.service.ts
@@ -1,17 +1,17 @@
 import { error } from "console";
 import { UserRepository } from "../repository/user.repository";
 import { hashPassword, verifyPassword } from "./security.service";
+import { UserModel } from "src/core/models/user.model";
 
 class UserService {
     private userRepository: UserRepository;
 
-    async signUp(userData: any): Promise<void> {
+    async signUp(userData: UserModel): Promise<void> {
 
         if (this.userRepository.findOne(userData)) {
             throw new error('User already exists')
         }
 
-        // Creating a hash for the user's password
         const hashedPassword = await hashPassword(userData.password);
         const [salt, hash] = hashedPassword.split('.');
         userData.password = hash
