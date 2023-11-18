fc1957f4df9d30e598c28aa84d25b9d695a871cf
tcardosoc <tcardosoc.dev@gmail.com>
2023-11-18 10:54:22 -0300

    feat(*): add email service to send activation email

--- src/infra/services/auth.service.ts
+++ src/infra/services/auth.service.ts
@@ -1,6 +1,8 @@
+import { UserModel } from "src/core/models/user.model";
 import { UserRepository } from "../repository/user.repository";
 import Cryptography from "./security.service";
 import { ISignUpData } from "src/core/interfaces/user.interfaces";
+import authMailService from "./auth-mail.service";
 
 class UserService {
     private userRepository: UserRepository;
@@ -9,17 +11,29 @@ class UserService {
         this.userRepository = new UserRepository()
     }
 
-    async signUp(userData: ISignUpData): Promise<void> {
+    async signUp(data: ISignUpData): Promise<void> {
+        const alreadySignedUp = await this.userRepository.findOne({ email: data.email })
 
-        if (this.userRepository.findOne(userData)) {
+        if (alreadySignedUp) {
             throw new Error('User already exists')
         }
 
-        const hashedPassword = await Cryptography.hashPassword(userData.password);
+        const hashedPassword = await Cryptography.hashPassword(data.password);
         const [salt, hash] = hashedPassword.split('.');
-        userData.password = hash
+        data.password = hash
 
-        const newUser = await this.userRepository.create(userData);
+        const userCreated = await this.userRepository.create(data);
+
+        const accountActivationToken = Cryptography.generateToken()
+
+        authMailService.accountActivation({
+            to: userCreated.email,
+            templateData: {
+                firstName: userCreated.firstName,
+                email: userCreated.email,
+                activationUrl: 'for a while nothing to be rendered'
+            }
+        })
 
         return
     }
