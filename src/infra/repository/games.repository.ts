e6e5c2001ee5fd15131a50dd90acfe061169b334
tcardosoc <tcardosoc.dev@gmail.com>
2023-11-18 15:57:27 -0300

    refactor(TASK): add new outputs responses with a customization

--- src/infra/repository/games.repository.ts
+++ src/infra/repository/games.repository.ts
@@ -1,7 +1,7 @@
-import { ICreateGamesRepository, IFindAllWhereGamesRepository, IFindOneWhereGamesRepository, IRemoveWhereGamesRepository, IUpdateGamesRepository, IUpdateWhereGamesRepository } from "src/core/interfaces/games.interfaces";
+import { ICreateGamesRepository, IFindAllWhereGamesRepository, IFindOneWhereGamesRepository, IRemoveWhereGamesRepository, IResponseCreateGamesRepository, IResponseDeleteGamesRepository, IResponseFindAllGamesRepository, IResponseFindOneGamesRepository, IResponseUpdateGamesRepository, IUpdateGamesRepository, IUpdateWhereGamesRepository } from "src/core/interfaces/games.interfaces";
 import { GamesModel } from "src/core/models/games.model";
 import { AppDataSource } from "src/data-source";
-import { DeleteResult, Repository, UpdateResult } from "typeorm";
+import { Repository } from "typeorm";
 
 export class GamesRepository {
 
@@ -11,23 +11,23 @@ export class GamesRepository {
         this.userDatabase = AppDataSource.getRepository(GamesModel)
     }
 
-    async create(userData: ICreateGamesRepository): Promise<ICreateGamesRepository> {
+    async create(userData: ICreateGamesRepository): Promise<IResponseCreateGamesRepository> {
         return await this.userDatabase.save(userData);
     }
 
-    async update(where: IUpdateWhereGamesRepository, userData: IUpdateGamesRepository): Promise<UpdateResult> {
+    async update(where: IUpdateWhereGamesRepository, userData: IUpdateGamesRepository): Promise<IResponseUpdateGamesRepository> {
         return await this.userDatabase.update(where, userData);
     }
 
-    async findOne(where: IFindOneWhereGamesRepository): Promise<GamesModel> {
+    async findOne(where: IFindOneWhereGamesRepository): Promise<IResponseFindOneGamesRepository> {
         return await this.userDatabase.findOne(where);
     }
 
-    async findAll(where: IFindAllWhereGamesRepository): Promise<GamesModel[]> {
+    async findAll(where: IFindAllWhereGamesRepository): Promise<IResponseFindAllGamesRepository[]> {
         return await this.userDatabase.find(where)
     }
 
-    async remove(where: IRemoveWhereGamesRepository): Promise<DeleteResult> {
+    async remove(where: IRemoveWhereGamesRepository): Promise<IResponseDeleteGamesRepository> {
         return await this.userDatabase.delete(where)
     }
 }
