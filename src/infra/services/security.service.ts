2d27a88b74a5de7fad7cc1219ff3804347967f1d
tcardosoc <tcardosoc.dev@gmail.com>
2023-11-18 10:53:38 -0300

    task(*): add new function to generate a token

--- src/infra/services/security.service.ts
+++ src/infra/services/security.service.ts
@@ -1,6 +1,5 @@
 import * as crypto from 'crypto';
 
-
 class Cryptography {
     static async hashPassword(password: string): Promise<string> {
         const salt = crypto.randomBytes(16).toString('hex');
@@ -12,8 +11,16 @@ class Cryptography {
     static async verifyPassword(password: string, hashedPassword: string): Promise<boolean> {
         const [salt, hash] = hashedPassword.split('.');
         const newHash = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha256').toString('hex');
-    
+
         return newHash === hash;
+    }
+
+    static generateToken(): string {
+        const valorAleatorio = crypto.randomBytes(20).toString('hex');
+
+        const hash = crypto.createHash('sha256').update(valorAleatorio).digest('hex');
+
+        return hash;
     }
 }
 
